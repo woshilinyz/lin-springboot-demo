3f56f49502db8b5a26eaa08ad4b80089ba7dd50c
linyz <529029157@qq.com>
2020-04-14 20:45:45 +0800

    init springboot redis

---
+++ lin-springboot-demo-redis/src/main/java/com/lin/linspringbootdemoredis/LinSpringbootDemoRedisApplication.java
@@ -0,0 +1,13 @@
+package com.lin.linspringbootdemoredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinSpringbootDemoRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinSpringbootDemoRedisApplication.class, args);
+	}
+
+}
