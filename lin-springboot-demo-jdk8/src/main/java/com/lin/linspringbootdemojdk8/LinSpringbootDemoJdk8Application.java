02ca1a24ba29693cf206d0974a74b34d8f7361f1
linyz <529029157@qq.com>
2020-04-14 21:44:13 +0800

    init jdk8

---
+++ lin-springboot-demo-jdk8/src/main/java/com/lin/linspringbootdemojdk8/LinSpringbootDemoJdk8Application.java
@@ -0,0 +1,13 @@
+package com.lin.linspringbootdemojdk8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinSpringbootDemoJdk8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinSpringbootDemoJdk8Application.class, args);
+	}
+
+}
